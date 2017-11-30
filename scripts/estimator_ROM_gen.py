d2b5440ae0d732ed51377bed2ec97f539195e081
Kirill Polzounov <kirill.polzounov@ucalgary.ca>
2017-11-29 18:04:56 -0700

    More features for estimator

--- scripts/estimator_ROM_gen.py
+++ scripts/estimator_ROM_gen.py
@@ -1,63 +1,144 @@
 from __future__ import print_function
 import numpy as np
 
-binary_width = 9
-
-x = np.linspace(0, 2**binary_width-1, 2**binary_width) # voltage
-#y = (-1.6747)*x**5 + (16.9)*x**4 - (67.951)*x**3 + (137.4)*x**2 - (144.71)*x+72.779
-# TODO: this function is wrong! fix this!
-y = (-5E-06)*(x)**5 + (6E-04)*(x)**4 - (0.028)*(x)**3 + (0.662)*(x)**2 - (8.254)*(x) + 51.286 # distance
-np.clip(y, 0, ((2**binary_width-1)/10.), out=y) # limit distances to 0 - 51.1 cm
-
-# Takes ndarray converts to binary str, remove the '0b', & left fill with 0s to proper width
-bin_and_fill = lambda a: bin(a)[2:].zfill(binary_width)
-vectorized_bin = np.vectorize(bin_and_fill)
-
-x_int = x.astype(np.int)
-y_int = (y*10.).astype(np.int) # Take value in cm and convert to mm (assume between 0-51.1cm)
-
-x_bin = vectorized_bin(x_int) # this should be 0 to 3.3 -> represented a a logic vec between 0s and 1s
-y_bin = vectorized_bin(y_int) # this should be 0 to 30
-
-addresses = x_bin
-values = y_bin
 
 
-
-rom_str = 'type ROM is array (0 to {}) of STD_LOGIC_VECTOR({} downto 0);\n'.format(2**binary_width-1, binary_width-1)
-rom_str += '    constant estimator_rom: ROM := ('
-for i, addr in enumerate(addresses):
-	rom_str += '("{}"), '.format(values[i])
-rom_str = rom_str[:-2] + ');'
-
-code_string = 'library IEEE;\n\
-use IEEE.STD_LOGIC_1164.ALL;\n\
-use IEEE.STD_LOGIC_ARITH.ALL;\n\
-use IEEE.STD_LOGIC_UNSIGNED.ALL;\n\
-\n\
-entity estimator is\n\
-        generic (width: integer := {});\n\
-        Port (\n\
-                clk      : in  STD_LOGIC;\n\
-                reset    : in  STD_LOGIC;\n\
-                voltage  : in  STD_LOGIC_VECTOR(width-1 downto 0);\n\
-                distance : out STD_LOGIC_VECTOR(width-1 downto 0)\n\
-         );\n\
-end estimator;\n\
-\n\
-architecture Behavioral of estimator is\n\
-    {}\n\
-\n\
-begin\n\
--- Internal processes ----------------------------------------------------------\n\
-    distance <= estimator_rom(CONV_INTEGER(UNSIGNED(voltage)));\n\
-\n\
-end Behavioral;\n\
-\n\
-'.format(binary_width, rom_str)
+bcd_lookup_table = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001'
+}
 
 
-filename = '../estimator/estimator.vhd'
-with open(filename, 'w') as f:
-	f.write(code_string)
+def num_to_bcd(i, num_digits):
+    str_i = str(i).replace('.', '')
+    str_i = str_i[0:min(len(str_i), num_digits)]
+    for _ in range(num_digits - len(str_i)):
+        str_i = '0' + str_i
+    str_bcd = ''
+    for digit in str_i:
+        str_bcd += bcd_lookup_table[digit]
+    return str_bcd
+
+
+def get_ROM_str(key, value, width):
+    '''Take a key & value pair, and return a string of a VHDL ROM'''
+    rom_str = '    constant {}: ROM := ('.format(key)
+    for i in range(2**width):
+        rom_str += '("{}"), '.format(value[i])
+    rom_str = rom_str[:-2] + ');\n'
+    return rom_str
+
+
+def write_estimator_vhd(filename, values, binary_width=9, num_digits=3):
+
+    # dictionary, key is that name of the ROM, val is values of ROM. All ROMs are same size
+    keys = [key for key in values.iterkeys()]
+    #---------------------------------------------------------------------------
+    code_string = 'library IEEE;\n\
+    use IEEE.STD_LOGIC_1164.ALL;\n\
+    use IEEE.STD_LOGIC_ARITH.ALL;\n\
+    use IEEE.STD_LOGIC_UNSIGNED.ALL;\n\
+    \n\
+    entity estimator is\n\
+            generic (width: integer := {});\n\
+            Port (\n\
+                    clk        : in  STD_LOGIC;\n\
+                    reset      : in  STD_LOGIC;\n'.format(binary_width)
+    #---------------------------------------------------------------------------
+    # Set which ROM to use as an input
+    for key in keys:
+        code_string += '                    en_{}: in STD_LOGIC;\n'.format(key)
+
+
+    #---------------------------------------------------------------------------
+    # Set the size of distance (several binary coded digits concatenated together)
+    code_string += '                    voltage    : in  STD_LOGIC_VECTOR(width-1 downto 0);\n\
+                    distance   : out STD_LOGIC_VECTOR({}*4-1 downto 0)\n\
+            );\n\
+    end estimator;\n\
+    \n\
+    architecture Behavioral of estimator is\n'.format(num_digits)
+    code_string += '    type ROM is array (0 to {}) of STD_LOGIC_VECTOR({} downto 0);\n'.format(2**binary_width-1, binary_width-1)
+
+    
+
+    #---------------------------------------------------------------------------
+    # Add all ROM definitions here
+    for k, v in values.iteritems():
+        code_string += get_ROM_str(k,v, binary_width)
+
+
+    #---------------------------------------------------------------------------
+    code_string += '\n\
+    begin\n\
+    -- Internal processes ----------------------------------------------------------\n\
+    select_rom : process('
+
+
+    #---------------------------------------------------------------------------
+    # Code for selecting which ROM is used
+    code_string += ''.join([' en_{}, '.format(key) for key in keys])[1:-2] # Remove trailing comma & space
+    
+    code_string += ')\n\
+    begin\n\
+        if (en_{} = \'1\') then\n\
+            distance <= {}(CONV_INTEGER(UNSIGNED(voltage)));'.format(keys[0], keys[0])
+
+    for key in keys[1:]:
+        code_string += '\n        elsif (en_{} = \'1\') then\n\
+            distance <= {}(CONV_INTEGER(UNSIGNED(voltage)));'.format(key, key)
+
+    code_string += '\n        else\n\
+            distance <= '
+    code_string += ''.join([' & "1111"' for _ in range(num_digits)])[3:] + ';'
+
+    code_string += '\n        end if;\n\
+    end process ; -- select_rom\n\
+    \nend Behavioral;\n\
+    \n'
+    #---------------------------------------------------------------------------
+
+    with open(filename, 'w') as f:
+        f.write(code_string)
+
+
+def main(filename='../estimator/estimator.vhd',
+         binary_width=9, # Make a rom with 9 bits of accuracy
+         num_digits=3,   # The output bcd will be num_digits*4 bits
+         max_voltage=3.3,# FPGA voltage is 3.3v
+         # The actual function we're using
+         distance_func=lambda x:(3.2194*x**4 - 25.53*x**3 + 75.61*x**2 - 102.92*x + 62.383),
+         scales={'dists_cm': 1, 'dists_mm': 10, 'dists_in': 0.3937008}
+        ):
+    # Creates the function to go from voltages to distances
+    voltages = np.linspace(0, max_voltage, 2**binary_width) # voltages in volts
+    distances = distance_func(voltages) # distances in cm
+
+    # No negative distances are allowed, so clip negatives
+    distances = np.maximum(distances, 0)
+
+    # Takes a list of numbers and converts them to bcd strings
+    vnum_to_bcd = np.vectorize(num_to_bcd)
+
+    # Get the actual ROMs as a dictionary
+    bcd_ROMs = {}
+    for name, scale in scales.iteritems():
+        bcd_ROMs[name] = vnum_to_bcd(distances * scale, num_digits)
+        print('\n\n\n\n{}\n\n{}'.format(name, distances * scale))
+        print('\n\n{}'.format(vnum_to_bcd(distances * scale, num_digits)))
+
+    # Write the file
+    write_estimator_vhd(filename, bcd_ROMs, binary_width=binary_width, num_digits=num_digits)
+
+
+if __name__ == '__main__':
+    main()
 
