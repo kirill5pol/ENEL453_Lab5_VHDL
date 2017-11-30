5e0630e28de50c955a90678eec2a209caa7441c1
Kirill Polzounov <kirill.polzounov@ucalgary.ca>
2017-11-29 20:58:11 -0700

    Update display every tenth of a second

--- vga/vga_module.vhd
+++ vga/vga_module.vhd
@@ -38,7 +38,7 @@ architecture Behavioral of vga_module is
             minutes_port    : out STD_LOGIC_VECTOR(4-1 downto 0); -- unused
             ten_minutes_port: out STD_LOGIC_VECTOR(3-1 downto 0); -- unused
             twentyfive_MHz  : out STD_LOGIC;
-            Hz              : out STD_LOGIC
+            daHz             : out STD_LOGIC -- update digits every tenth of a second
         );
     end component;
     component digits_box is
@@ -59,7 +59,7 @@ architecture Behavioral of vga_module is
 
 -- Internal Signals ------------------------------------------------------------
     -- Clock divider signals:
-    signal i_kHz, i_Hz, i_pixel_clk: std_logic;
+    signal i_kHz, i_daHz, i_pixel_clk: std_logic;
     -- Sync module signals:
     signal vga_blank : std_logic;
     signal scan_line_x, scan_line_y: STD_LOGIC_VECTOR(10 downto 0);
@@ -83,7 +83,7 @@ begin
                 ten_seconds_port => open,
                 minutes_port     => open,
                 ten_minutes_port => open,
-                Hz               => i_Hz
+                daHz              => i_daHz
         );
     VGA_SYNC: sync_signals_generator
         Port map(
@@ -96,13 +96,13 @@ begin
                 scan_line_y  => scan_line_y
         );
 
-    DELAY_DIGITS: process(i_Hz, reset) -- only update the digits once a second on the vga
+    DELAY_DIGITS: process(i_daHz, reset) -- only update the digits once a second on the vga
     begin
         if (reset = '1') then
             i_digit_tens <= "0000";
             i_digit_ones <= "0000";
             i_digit_tenths <= "0000";
-        elsif (rising_edge(i_Hz)) then
+        elsif (rising_edge(i_daHz)) then
             i_digit_tens <= distance_bcd(11 downto 8);
             i_digit_ones <= distance_bcd(7 downto 4);
             i_digit_tenths <= distance_bcd(3 downto 0);
