5b9beab57c62a39e815a23fe6b2985591daeb3d7
Kirill Polzounov <kirill.polzounov@ucalgary.ca>
2017-11-30 20:39:35 -0700

    Sig gen update

--- signal_gen/signal_gen.vhd
+++ signal_gen/signal_gen.vhd
@@ -27,11 +27,9 @@ architecture Behavioral of signal_gen is
 -- Internal Signals ------------------------------------------------------------
     signal i_voltage     : STD_LOGIC_VECTOR(9-1 downto 0) := (others => '0'); -- 9 is width
     signal i_voltage_prev: STD_LOGIC_VECTOR(9-1 downto 0) := (others => '0'); -- 9 is width
-
-    constant min_voltage : STD_LOGIC_VECTOR(9-1 downto 0) := (others => '0'); -- 9 is width
-    constant max_voltage : STD_LOGIC_VECTOR(9-1 downto 0) := (others => '1'); -- 9 is width
-
-    signal counter : STD_LOGIC_VECTOR (9-1 downto 0); -- update along with pwm DAC
+    signal current_voltage: STD_LOGIC_VECTOR(9-1 downto 0) := (others => '0'); -- 9 is width
+    signal counter : STD_LOGIC_VECTOR (12-1 downto 0); -- update along with pwm DAC
+    signal enable: STD_LOGIC := '0';
 
 
 begin
@@ -40,29 +38,41 @@ begin
     begin
         if( reset = '1') then
             counter <= (others => '0');
+            enable <= '0';
         elsif (rising_edge(clk)) then
+            if (counter = "111111111111") then
+                enable <=  '1';
+            elsif (enable = '1') then
+                enable <= '0';
+            end if;
             counter <= counter + '1';
         end if;
     end process;
 
-    check_real_voltage: process(clk, reset, counter) -- Todo: fix to remove bit bobble
+    check_real_voltage: process(clk, reset) -- Todo: fix to remove bit bobble
     begin
         if (reset = '1') then
-            voltage <= (others => '0');
+            --voltage <= (others => '0');
+            current_voltage <= (others => '0');
             i_voltage <= (others => '0');
             i_voltage_prev <= (others => '0');
 
-        elsif (rising_edge(clk) AND (counter(9-1 downto 0) = "111111111")) then
-            if (comparator = '0') then
-                voltage <= i_voltage_prev; -- Copy the voltage change
-                i_voltage <= (others => '0'); -- Reset the ramp
-            else
-                i_voltage <= i_voltage + 1;
-                i_voltage_prev <= i_voltage;
+        elsif (rising_edge(clk)) then
+            if (enable = '1') then
+                if (comparator = '1') then
+                    i_voltage <= i_voltage + 1;
+                    i_voltage_prev <= i_voltage;
+                else
+                    i_voltage <= (others => '0');
+                    current_voltage <= i_voltage_prev;
+                end if;
+
             end if;
 
         end if;
     end process ; -- check_real_voltage
+    
+    voltage <= current_voltage;
 
 -- Module instantiation --------------------------------------------------------
     PWM_DAC_OUTPUT: PWM_DAC
